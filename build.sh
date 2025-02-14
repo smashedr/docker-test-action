748708a026c09c7b3411c750136bc370b5a5d4b4
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-14 00:52:04 -0800

    Add build workflow

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build src --platform linux/amd64 --tag ghcr.io/smashedr/docker-test-action:latest
+docker build src --platform linux/amd64 --tag ghcr.io/smashedr/docker-test-action:v1
 act -j test --action-offline-mode "$@"
