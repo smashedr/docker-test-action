c997689b6c6197e4686854b07f57b35eaa999397
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-15 19:37:04 -0800

    Update Release Workflow

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-source version.txt
-docker build src --platform linux/amd64 --tag "ghcr.io/smashedr/docker-test-action:${VERSION}"
+docker build src --platform linux/amd64 --tag "ghcr.io/smashedr/docker-test-action:v1.0.0"
 act -j test -e event.json --action-offline-mode "$@"
