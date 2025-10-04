3e46f08b639070d5b418f8058832432fa94fdd01
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-10-03 23:53:33 -0700

    Update Version, Build and Dockerfile (#15)

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@
 [[ ! -f "action.yml" ]] && echo "Wrong Directory" && exit 1
 _IMAGE=$(grep -o 'ghcr.io/.*' action.yml | sed 's/".*//')
 echo "Using image: ${_IMAGE}"
-echo "Local Build" > src/version.txt
-docker build src --platform linux/amd64 --tag "${_IMAGE}"
-rm src/version.txt
+docker build --tag "${_IMAGE}" .
 act -j test -e event.json --action-offline-mode "$@"
